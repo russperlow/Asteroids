defb59764fa7911ee541bb4c8d694ec8c8268161
Russ Perlow <rep4975@rit.edu>
2017-03-09 12:11:18 -0500

    Created Initial Projected

---
+++ Asteroids/Asteroids/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    /// <summary>
+    /// A class used to create bullet objects
+    /// </summary>
+    class Bullet
+    {
+
+    }
+}
---
+++ Asteroids/Asteroids/Ship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    /// <summary>
+    /// The player/ship handles shooting, movement, drawing
+    /// </summary>
+    class Ship
+    {
+
+    }
+}
